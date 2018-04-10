b630644cf7d3af20d02dd9ab236289395075402b
Michał Sykutera <msykutera@gmail.com>
2018-04-10 19:29:33 +0200

    Set url

--- HtmlMinifier/Program.cs
+++ HtmlMinifier/Program.cs
@@ -8,6 +8,9 @@ namespace HtmlMinifier
         public static void Main(string[] args) => BuildWebHost(args).Run();
 
         public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+            WebHost.CreateDefaultBuilder(args)
+                   .UseUrls("http://localhost:4990")
+                   .UseStartup<Startup>()
+                   .Build();
     }
 }
